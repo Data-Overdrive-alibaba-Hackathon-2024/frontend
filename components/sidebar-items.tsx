cd8caa995bdd90dc13cd3788d419ae94aac2b62e
Aziz <muhaariq@gmail.com>
2024-09-01 10:27:27 +0700

    fix: remove unused code

--- components/sidebar-items.tsx
+++ components/sidebar-items.tsx
@@ -3,8 +3,6 @@
 import { Chat } from '@/lib/types'
 import { AnimatePresence, motion } from 'framer-motion'
 
-import { removeChat, shareChat } from '@/app/actions'
-
 import { SidebarActions } from '@/components/sidebar-actions'
 import { SidebarItem } from '@/components/sidebar-item'
 
@@ -16,27 +14,28 @@ export function SidebarItems({ chats }: SidebarItemsProps) {
   if (!chats?.length) return null
 
   return (
-    <AnimatePresence>
-      {chats.map(
-        (chat, index) =>
-          chat && (
-            <motion.div
-              key={chat?.id}
-              exit={{
-                opacity: 0,
-                height: 0
-              }}
-            >
-              <SidebarItem index={index} chat={chat}>
-                <SidebarActions
-                  chat={chat}
-                  removeChat={removeChat}
-                  shareChat={shareChat}
-                />
-              </SidebarItem>
-            </motion.div>
-          )
-      )}
-    </AnimatePresence>
+    <div></div>
+    // <AnimatePresence>
+    //   {chats.map(
+    //     (chat, index) =>
+    //       chat && (
+    //         <motion.div
+    //           key={chat?.id}
+    //           exit={{
+    //             opacity: 0,
+    //             height: 0
+    //           }}
+    //         >
+    //           <SidebarItem index={index} chat={chat}>
+    //             <SidebarActions
+    //               chat={chat}
+    //               removeChat={removeChat}
+    //               shareChat={shareChat}
+    //             />
+    //           </SidebarItem>
+    //         </motion.div>
+    //       )
+    //   )}
+    // </AnimatePresence>
   )
 }
