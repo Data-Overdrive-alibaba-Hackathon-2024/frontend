51d56b634cfd89ee31ed1a20cdc65d8c4da1d5b6
Aziz <muhaariq@gmail.com>
2024-09-01 13:26:44 +0700

    fix: remove lagi

--- app/(chat)/chat/[id]/layout.tsx
+++ app/(chat)/chat/[id]/layout.tsx
@@ -1,4 +1,4 @@
-import { Header } from '@/components/header'
+// import { Header } from '@/components/header'
 import { ChatbotHeader } from '@/components/new/chatbot-header'
 import { SidebarDesktop } from '@/components/sidebar-desktop'
 
